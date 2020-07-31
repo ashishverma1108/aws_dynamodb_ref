56939e4d23002957d9a3021fafa7b0f8935add9a
ashishverma1108 <averma03@syr.edu>
2020-07-30 22:09:35 -0400

    Update Dynamodb_cli_basics.sh

--- Dynamodb_cli_basics.sh
+++ Dynamodb_cli_basics.sh
@@ -2,3 +2,6 @@
 
 # List tables operation in current default region
 aws dynamodb list-tables --profile <profile_name>
+
+# Describe any tale in dyanamodb
+aws dynamodb describe-table --table-name <table name> --profile <profile name>
