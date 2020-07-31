a44e6dba27038260fd74720f2b76823e91c4d250
ashishverma1108 <averma03@syr.edu>
2020-07-30 22:15:21 -0400

    Update Dynamodb_cli_basics.sh

--- Dynamodb_cli_basics.sh
+++ Dynamodb_cli_basics.sh
@@ -5,3 +5,10 @@ aws dynamodb list-tables --profile <profile_name>
 
 # Describe any tale in dyanamodb
 aws dynamodb describe-table --table-name <table name> --profile <profile name>
+
+# Provide table definition as a json file while creating table
+aws dynamodb create-table --cli-input-json <filename.json or {inline json}> --profile <profile name>
+
+# Command to understand json structure expected by dynamodb cli while creating table. Use this to 
+# create file to be used for above command
+aws dynamodb create-table generate-cli-skeleton --profile <profile name>
